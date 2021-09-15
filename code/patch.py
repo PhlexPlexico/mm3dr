ebb108f44fe5eb0fd4e56052c22960a7a9fbd969
PhlexPlexico <flexing@phlexplexi.co>
2021-09-15 12:17:16 -0600

    Start of basic patching and removing Magikoopa.

--- code/patch.py
+++ code/patch.py
@@ -6,6 +6,7 @@ import json
 
 elf = sys.argv[1]
 result = subprocess.run([os.environ["DEVKITARM"] + r'/bin/arm-none-eabi-objdump', '--section-headers', elf], stdout=subprocess.PIPE)
+print(result)
 lines = str(result.stdout).split('\\n')
 sectionsInfo = [line.split()[1:6] for line in lines if line.split() and line.split()[0].isdigit()]
 sections = ((sec[0], int(sec[2],16), int(sec[4],16), int(sec[1],16)) for sec in sectionsInfo if int(sec[2],16) != 0)
@@ -15,7 +16,6 @@ desiredSymbols = ("rItemOverrides", "gSettingsContext", "gSpoilerData", "rScrubR
 
 nmResult = subprocess.run([os.environ["DEVKITARM"] + r'/bin/arm-none-eabi-nm', elf], stdout=subprocess.PIPE)
 nmLines = str(nmResult.stdout).split('\\n')
-print(nmLines)
 symbolsInfo = [line.split() for line in nmLines if len(line.split()) >= 3 and line.split()[2].replace("\\r", "") in desiredSymbols]
 symbols = {sym[2].replace("\\r", ""):hex(int(sym[0],16)) for sym in symbolsInfo}
 symbolsJson = json.dumps(symbols, indent=4)
