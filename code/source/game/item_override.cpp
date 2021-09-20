9a787670090f9cb2991016a79bff3b4281195508
PhlexPlexico <flexing@phlexplexi.co>
2021-09-20 15:50:10 -0600

    More GI table updates.

--- code/source/game/item_override.cpp
+++ code/source/game/item_override.cpp
@@ -20,7 +20,7 @@ extern "C" {
 
       // No override, use base game's item code
       // ItemOverride_Clear();
-      player->get_item_id_maybe = (u32) GetItemID::GI_UNK_18;
+      player->get_item_id_maybe = (u32) GetItemID::GI_BOMB_BAG_20;
       return;
     }
 }
