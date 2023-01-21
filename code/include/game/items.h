7448b1eadf91c08a674fccf3d3ab53935f0621d1
PhlexPlexico <PhlexPlexico@users.noreply.github.com>
2023-01-21 21:56:14 +0000

    Clang-format changes exist. Committing.

--- code/include/game/items.h
+++ code/include/game/items.h
@@ -225,7 +225,7 @@ namespace game {
       ItemId::CaptainHat,  ItemId::GiantMask,        ItemId::FierceDeityMask};
 
   // Ordered from inventory.cpp.
-  const u32 MaskSlotsOrdered[] = {0, 1, 3, 2, 4, 5, 6, 7, 8, 9, 10, 11,
+  const u32 MaskSlotsOrdered[] = {0,  1,  3,  2,  4,  5,  6,  7,  8,  9,  10, 11,
                                   12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
 
   constexpr bool ItemIsBottled(ItemId item) {
