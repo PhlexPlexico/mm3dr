c76212860a474d0c2430ffe4d5d017f00168181d
phlexplexico <flexing@phlexplexi.co>
2023-05-09 20:38:10 -0600

    Bombers notebook goes zoom

---
+++ code/source/asm/animation_patches.s
@@ -0,0 +1,31 @@
+.arm
+
+.section .patch_ShorterAnimationFail
+.global patch_ShorterAnimationFail
+patch_ShorterAnimationFail:
+  b 0x5B1870
+
+.section .patch_ShorterAnimationOpenOne
+.global patch_ShorterAnimationOpenOne
+patch_ShorterAnimationOpenOne:
+  .float 0.01
+
+.section .patch_ShorterAnimationOpenTwo
+.global patch_ShorterAnimationOpenTwo
+patch_ShorterAnimationOpenTwo:
+  .float 0.01
+
+.section .patch_ShorterAnimationOpenThree
+.global patch_ShorterAnimationOpenThree
+patch_ShorterAnimationOpenThree:
+  .float 0.01
+
+.section .patch_ShorterAnimationOpenFour
+.global patch_ShorterAnimationOpenFour
+patch_ShorterAnimationOpenFour:
+  .float 0.01
+
+.section .patch_ShorterAnimationOpenFive
+.global patch_ShorterAnimationOpenFive
+patch_ShorterAnimationOpenFive:
+  .float 0.01
