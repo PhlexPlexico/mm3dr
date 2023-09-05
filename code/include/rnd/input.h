c694662980583eb1b1da2ca1e083ade40f6b1334
Phlex <flexing@phlexplexi.co>
2023-09-05 09:22:50 -0600

    Formatting.
    Fix menu not showing with buffer issues.

--- code/include/rnd/input.h
+++ code/include/rnd/input.h
@@ -5,22 +5,22 @@
 #include "z3d/z3DVec.h"
 
 namespace rnd {
-typedef struct {
-  btn_t cur;
-  btn_t up;
-  btn_t pressed;
-  btn_t old;
-} InputContext;
+  typedef struct {
+    btn_t cur;
+    btn_t up;
+    btn_t pressed;
+    btn_t old;
+  } InputContext;
 
-void Input_Update(void);
-u32 Input_WaitWithTimeout(u32 msec, u32 closingButton);
-u32 Input_Wait(void);
+  void Input_Update(void);
+  u32 Input_WaitWithTimeout(u32 msec, u32 closingButton);
+  u32 Input_Wait(void);
 
 // Use the shared game HID.
 #define real_hid_addr 0x007B2D34
 #define real_hid (*(hid_mem_t**)real_hid_addr)
 
-extern "C" InputContext rInputCtx;
+  extern "C" InputContext rInputCtx;
 
-} // namespace rnd
+}  // namespace rnd
 #endif
